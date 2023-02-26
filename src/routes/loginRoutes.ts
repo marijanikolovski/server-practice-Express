3b747b491e23715ee18b725ab4aeaf3044d4386b
marijanikolovski <m.kuzeljevic@gmail.com>
2023-02-26 16:45:55 +0100

    Protecting Routes

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,18 @@
-import { Router,Request, Response } from "express";
+import { Router,Request, Response, NextFunction } from "express";
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined}
 }
 
+function requireAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.session && req.session.loggedIn) {
+    next();
+    return;
+  }
+
+  res.status(403);
+  res.send('Not permitted');
+}
 const router = Router();
 
 router.get('/login', (req: RequestWithBody, res: Response) => {
@@ -55,5 +64,9 @@ router.get('/logout', (req: Request, res: Response) => {
   req.session = undefined;
   res.redirect('/');
 })
+
+router.get('/protected', requireAuth, (req: Request, res: Response) => {
+  res.send('Welcome to protected route, logged in user.')
+});
 
 export { router };
