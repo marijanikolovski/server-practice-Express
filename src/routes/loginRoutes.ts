588427f4c0acdec2010940e339a6019c3563ad34
marijanikolovski <m.kuzeljevic@gmail.com>
2023-02-26 14:14:19 +0100

    Login implemented

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,9 +1,37 @@
-import { Router } from "express";
+import { Router,Request, Response } from "express";
+
+interface RequestWithBody extends Request {
+  body: { [key: string]: string | undefined}
+}
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.send('hi there')
+router.get('/login', (req: RequestWithBody, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <div>
+        <button>Subimt</buttons>
+      </div>
+    </form>
+  `)
+})
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+  const { email, password } = req.body;
+
+    if (email) {
+      res.send(email.toUpperCase());
+    } else {
+      res.send('You must provide an email')
+    }
 })
 
 export { router };
