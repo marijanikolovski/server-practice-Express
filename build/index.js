19de4e8172455a0f9001b1fed654c42335917610
marijanikolovski <m.kuzeljevic@gmail.com>
2023-02-27 23:06:15 +0100

    Fixing Routes

--- build/index.js
+++ build/index.js
@@ -4,15 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const loginRoutes_1 = require("./routes/loginRoutes");
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_session_1 = __importDefault(require("cookie-session"));
 const AppRouter_1 = require("./AppRouter");
 require("./controllers/LoginController");
+require("./controllers/RootController");
 const app = (0, express_1.default)();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_session_1.default)({ keys: ['fdkfj'] }));
-app.use(loginRoutes_1.router);
 app.use(AppRouter_1.AppRouter.getInstance());
 app.listen(3000, () => {
     console.log('Listenitn on port 3000');
